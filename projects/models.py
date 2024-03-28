412d4722189d2677488867c57e6b0c875bea38f7
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-03-28 19:42:26 +0530

    project model is  added

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField(null = True, blank = True)
+    demo_link = models.CharField(max_length =2000, null = True, blank = True)
+    source_line = models.CharField(max_length =2000, null = True, blank = True)
+    created = models.DateTimeField(auto_now_add= True)
+    id = models.UUIDField(primary_key = True, editable = False, unique = True, default=uuid.uuid4)
+
+    def __str__(self):
+        return self.title
