6c277b35f0bd099e11f6c950e848e2d5785df285
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-04-01 16:29:10 +0530

    Review and Tag models were added and cardinality is established between project, tag and review.

--- projects/models.py
+++ projects/models.py
@@ -7,8 +7,33 @@ class Project(models.Model):
     description = models.TextField(null = True, blank = True)
     demo_link = models.CharField(max_length =2000, null = True, blank = True)
     source_line = models.CharField(max_length =2000, null = True, blank = True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default = 0, null = True, blank = True)
+    vote_ratio = models.IntegerField(default = 0, null = True, blank = True)
     created = models.DateTimeField(auto_now_add= True)
     id = models.UUIDField(primary_key = True, editable = False, unique = True, default=uuid.uuid4)
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'UP VOTE'),
+        ('down', 'DOWN VOTE'),
+    )
+    text = models.TextField(blank = True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    vote = models.CharField(max_length = 200, choices = VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add = True)
+    id = models.UUIDField(primary_key = True, editable = False, unique = True, default=uuid.uuid4)
+
+    def __str__(self):
+        return self.vote
+
+class Tag(models.Model):
+    name = models.CharField(max_length = 200)
+    created = models.DateTimeField(auto_now_add = True)
+    id = models.UUIDField(primary_key = True, editable = False, unique = True, default=uuid.uuid4)
+
+    def __str__(self):
+        return self.name
