910062eda05e810e7ce8de8fb36e143a713bc9a6
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-04-02 22:26:36 +0530

    explored query set, read some data from the database and used it to  generate dynamic data in the template

--- projects/views.py
+++ projects/views.py
@@ -1,33 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-projectsList = [
-    {
-        'id':'1',
-        'title': 'Ecommerce website',
-        'description': 'Costumer can buy products here'
-    },
-    {
-        'id':'2',
-        'title': 'Portfolio website',
-        'description': 'This is the project where i built my portfolio'
-    },
-    {
-        'id':'3',
-        'title': 'Social network',
-        'description': 'A app where people can interact with eachother'
-    },
-]
+from .models import Project
 
 # Create your views here.
 def projects(request):
     msg = "Below are the list of all the projects"
-    number = 10
-    return render(request, 'projects/projects.html', context={'message':msg, 'number':number, 'projects': projectsList})
+    projectsList = Project.objects.all()
+    return render(request, 'projects/projects.html', context={'message':msg, 'projects': projectsList})
 
 def project(request, pk):
-    projectObj = None
-    for project in projectsList:
-        if project['id'] == pk:
-            projectObj = project
-    return render(request, 'projects/single-project.html', context = {'project':projectObj})
+    projectObj = Project.objects.get(id = pk)
+    tags = projectObj.tags.all()
+    return render(request, 'projects/single-project.html', context = {'project':projectObj, 'tags': tags})
