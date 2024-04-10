097ff1acfeddf84ff061e04918bf6b7badbe6cdc
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-04-10 20:21:59 +0530

    used ModelForm

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 def projects(request):
@@ -12,3 +13,8 @@ def project(request, pk):
     projectObj = Project.objects.get(id = pk)
     tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html', context = {'project':projectObj, 'tags': tags})
+
+def createProject(request):
+    form = ProjectForm()
+    context= {'form' : form}
+    return render(request, 'projects/project_form.html', context)
