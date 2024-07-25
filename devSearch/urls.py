20328b2775e6be52c7c5d45ca5dd3370e81f910a
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-25 19:38:22 +0530

    added comments in urls.py

--- devSearch/urls.py
+++ devSearch/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # username: vbsanjay password_hint: starts with H and ends with 2
     path('', include('projects.urls')),
 ]
