65fe739778f54c8c0b34ddda02e214de94a0fe83
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-27 23:19:30 +0530

    user could do crud on projects

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Project
 from .forms import ProjectForm
 
@@ -14,7 +14,28 @@ def project(request, pk):
     tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html', context = {'project':projectObj, 'tags': tags})
 
-def create_project(request):
+def createProject(request):
     form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
     return render(request, 'projects/project_form.html', context = {'form': form})
 
+def updateProject(request, pk):
+    item = Project.objects.get(id=pk)
+    form = ProjectForm(instance=item)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    return render(request, 'projects/project_form.html', context = {'form': form})
+
+def deleteProject(request, pk):
+    item = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('projects')
+    return render(request, 'projects/delete_object.html', context= {'project': item})
