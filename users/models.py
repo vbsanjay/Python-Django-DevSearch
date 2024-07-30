4aeb44281258e6dd7bc85a8e8afc33e21c4724c9
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-07-30 22:15:37 +0530

    updated user profile

--- users/models.py
+++ users/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
     short_into = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to = 'profiles/', default = 'profiles/user-default.png')
