4cb460aa4977ebd9a1de5f23230461a6c66521ff
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-05 15:31:37 +0530

    Theme installed for login and signup page

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -2,3 +2,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  hljs.highlightAll();
+
+  let alertWrapper = document.querySelector('.alert')
+  let alertClose = document.querySelector('.alert__close')
+
+  if (alertWrapper && alertClose) {
+    alertClose.addEventListener('click', () => {
+      alertWrapper.style.display = 'none'
+    })
+  }
+});
