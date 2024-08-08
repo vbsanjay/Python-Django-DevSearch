b0fceaea3298487835ed8dbc66573aa05c7ed630
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-08 12:24:00 +0530

    pagination implemented

--- projects/utils.py
+++ projects/utils.py
@@ -1,5 +1,36 @@
 from .models import Project, Tag
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+def paginateProject(request, projectsList, results):
+
+    page = request.GET.get('page')
+    results = 3
+    paginator = Paginator(projectsList, results)
+
+    try:
+        projectsList = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        projectsList = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        projectsList = paginator.page(page)
+
+    leftIndex = (int(page) - 4)
+
+    if leftIndex < 1:
+        leftIndex = 1
+
+    rightIndex = (int(page) + 5)
+
+    if rightIndex  > paginator.num_pages:
+        rightIndex = paginator.num_pages + 1
+    
+    custom_range = range(leftIndex, rightIndex)
+
+    return custom_range, projectsList
 
 def searchProjects(request):
     search_query = ''
--- projects/views.py
+++ projects/views.py
@@ -1,43 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Project, Tag
 from .forms import ProjectForm
-from .utils import searchProjects
+from .utils import searchProjects, paginateProject
 
 # Create your views here.
 def projects(request):
     projectsList, search_query = searchProjects(request)
-
-    page = request.GET.get('page')
     results = 3
-    paginator = Paginator(projectsList, results)
-
-    try:
-        projectsList = paginator.page(page)
-    except PageNotAnInteger:
-        page = 1
-        projectsList = paginator.page(page)
-    except EmptyPage:
-        page = paginator.num_pages
-        projectsList = paginator.page(page)
-
-    leftIndex = (int(page) - 4)
-
-    if leftIndex < 1:
-        leftIndex = 1
-
-    rightIndex = (int(page) + 5)
-
-    if rightIndex  > paginator.num_pages:
-        rightIndex = paginator.num_pages + 1
-    
-    custom_range = range(leftIndex, rightIndex)
+    custom_range, projectsList = paginateProject(request, projectsList, results)
 
     context = {'projects': projectsList, 'search_query': search_query, 
-               'paginator':paginator, 'custom_range': custom_range}
+            'custom_range': custom_range}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
