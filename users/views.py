d9ea9f46ee4e4f27bf4a45ed9a82c1ced917e3fe
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-08 15:56:31 +0530

    pagination implementation on profiles - backend

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from .utils import searchProfiles
+from .utils import searchProfiles, paginateProfiles
 
 def loginUser(request):
     page = 'login'
@@ -65,7 +65,10 @@ def registerUser(request):
 def profiles(request):
     profiles, search_query = searchProfiles(request)
     
-    context = {'profiles': profiles, 'search_query':search_query}
+    custom_range, profiles = paginateProfiles(request, profiles, 3)
+
+    context = {'profiles': profiles, 'search_query':search_query, 'custom_range': custom_range}
+
     return render(request, 'users/profiles.html', context)
 
 def userProfile(request, pk):
