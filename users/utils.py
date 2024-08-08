e9def13798bb1203f84d0ef892f4892c3ee5e9b5
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-08 15:55:15 +0530

    pagination of profiles refactored to utils

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,35 @@
 from .models import Profile, Skill
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+def paginateProfiles(request, profiles, results):
+
+    page = request.GET.get('page')
+    paginator = Paginator(profiles, results)
+
+    try:
+        profiles = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        profiles = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        profiles = paginator.page(page)
+
+    leftIndex = (int(page) - 4)
+
+    if leftIndex < 1:
+        leftIndex = 1
+
+    rightIndex = (int(page) + 5)
+
+    if rightIndex  > paginator.num_pages:
+        rightIndex = paginator.num_pages + 1
+    
+    custom_range = range(leftIndex, rightIndex)
+
+    return custom_range, profiles
 
 def searchProfiles(request):
     search_query = ''
@@ -13,7 +43,7 @@ def searchProfiles(request):
     # from django.db.models import Q this will help us to return data if search value is present either in name or short intrp
     profiles = Profile.objects.distinct().filter(Q(name__icontains=search_query) | 
                                       Q(short_intro__icontains=search_query) |
-                                      Q(skill__in=skills)) # skill__in=skills, this syntax for child object
+                                      Q(skill__in=skills)).order_by('name') # skill__in=skills, this syntax for child object
     
     
     return profiles, search_query
