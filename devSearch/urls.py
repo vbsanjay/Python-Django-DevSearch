61d249f1795eee2ffa20c762de5e3c750a41efc3
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-10 13:15:59 +0530

    rest password featured setup is done. mvp feature implement partially.

--- devSearch/urls.py
+++ devSearch/urls.py
@@ -3,10 +3,24 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls), # username: vbsanjay password_hint: starts with H and ends with 2
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), 
+         name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), 
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), 
+         name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name="password_reset_complete"),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
