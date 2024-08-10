98234315e2e74b0fb8983a95b9b94f156aa965f3
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-10 15:50:52 +0530

    theme installed for password reset pages

--- devSearch/urls.py
+++ devSearch/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), 
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"), 
          name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), 
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"), 
          name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), 
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"), 
          name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"),
          name="password_reset_complete"),
 
 ]
