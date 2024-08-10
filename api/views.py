cb804c71f87f57867c5b1ab276b355fa9ecf207e
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-10 18:17:47 +0530

    serialization is done

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -15,3 +17,15 @@ def getRoutes(request):
 
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getProjects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getProject(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
