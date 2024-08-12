dd5f9910837768c314cf7e4a5f47b918221de57a
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-12 06:51:40 +0530

    big fix: even if user delete default image the page still loads

--- projects/models.py
+++ projects/models.py
@@ -24,6 +24,14 @@ class Project(models.Model):
         ordering = ['-vote_ratio', '-vote_total', 'title']
     
     @property
+    def imageURL(self):
+        try:
+            url = self.featured_image.url
+        except:
+            url = ''
+        return url
+
+    @property
     def reviewers(self):
         queryset = self.review_set.all().values_list('owner__id', flat=True)
         return queryset
--- users/models.py
+++ users/models.py
@@ -23,6 +23,14 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.username)
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.profile_image.url
+        except:
+            url = ''
+        return url
 
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
