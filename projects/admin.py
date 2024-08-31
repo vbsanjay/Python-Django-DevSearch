9f4abdda14ef32bc4f319a1c35aaf8e95e1111fc
vbsanjay <72456646+vbsanjay@users.noreply.github.com>
2024-08-31 20:34:21 +0530

    Added few comments for clarity

--- projects/admin.py
+++ projects/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Project, Review, Tag
 
+# By registering these models, you’ll be able to add, edit, and delete instances of these models from the Django admin dashboard. 
 admin.site.register(Project)
 admin.site.register(Review)
 admin.site.register(Tag)
--- projects/models.py
+++ projects/models.py
@@ -8,14 +8,16 @@ class Project(models.Model):
         Profile, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length = 200)
     description = models.TextField(null = True, blank = True)
-    featured_image = models.ImageField(null = True, blank = True, default = 'default.jpg')
+    featured_image = models.ImageField(null = True, blank = True,
+                                        default = 'default.jpg') # default.jpg in the root of the MEDIA_ROOT directory.
     demo_link = models.CharField(max_length =2000, null = True, blank = True)
     source_link = models.CharField(max_length =2000, null = True, blank = True)
     tags = models.ManyToManyField('Tag', blank=True)
     vote_total = models.IntegerField(default = 0, null = True, blank = True)
     vote_ratio = models.IntegerField(default = 0, null = True, blank = True)
     created = models.DateTimeField(auto_now_add= True)
-    id = models.UUIDField(primary_key = True, editable = False, unique = True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key = True, editable = False, unique = True,
+                           default=uuid.uuid4)
 
     def __str__(self):
         return self.title
@@ -23,6 +25,7 @@ class Project(models.Model):
     class Meta:
         ordering = ['-vote_ratio', '-vote_total', 'title']
     
+
     @property
     def imageURL(self):
         try:
